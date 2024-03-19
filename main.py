328bdef50e8073f8534d9ef9edb29b4957845b07
Asp1kkk <a5p1k@yandex.ru>
2024-03-19 19:20:02 +0300

    END

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from functions.functions import crawl
-from database.queries import createQuery, insertQuery
 import sqlite3
 
 if __name__ == "__main__":
 	url = "https://www.restoran.ru/msk/catalog/restaurants/kitchen/russian/"
+	createQuery = """
+	CREATE TABLE "restaurants" (
+		"id"	INTEGER NOT NULL UNIQUE,
+		"name"	INTEGER,
+		"rating" NUMERIC(2,1),
+		"bill"	TEXT,
+		"tags" TEXT,
+		PRIMARY KEY("id" AUTOINCREMENT)
+	);
+	"""
+
+	insertQuery = """
+	INSERT INTO restaurants (name, rating, bill, tags) VALUES (?, ?, ?, ?)
+	"""
+
 	restaurants = crawl(url, 18)
 	
 	con = sqlite3.connect("./database/restaurants.db")
