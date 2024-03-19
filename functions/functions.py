8aa9e5b7d4cc68fcb5d568b0ea5cc36c46df68e7
Asp1kkk <a5p1k@yandex.ru>
2024-03-19 18:13:22 +0300

    getRate function created

--- functions/functions.py
+++ functions/functions.py
@@ -1,6 +1,6 @@
 import bs4
 
-def getAverageBill(item: bs4.element.Tag) -> str:
+def getBill(item: bs4.element.Tag) -> str:
 	bill_span = item.find("span", {"class": "average-bill"})
 	average_bill = bill_span.get_text().replace(".", "") if bill_span != None else None
 	return average_bill
@@ -14,5 +14,13 @@ def getTags(item: bs4.element.Tag) -> list:
 	cuisine_text = cuisine_span.find_parent().get_text()
 	cuisine_text = cuisine_text.replace("Кухня:  ", "").replace('\xa0', ' ')
 	cuisine_list = cuisine_text.split(', ')
-	
+
 	return cuisine_list
+
+def getRate(item: bs4.element.Tag) -> float:
+	dropdown_div = item.find("div", {"class": "dropdown"})
+	span_element = dropdown_div.find("span")
+	text_content = span_element.get_text()
+	float_value = float(text_content)
+
+	return float_value
