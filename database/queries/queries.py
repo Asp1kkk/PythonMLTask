73dd3bb35bffc22f14bd280234618d47a2493e86
Asp1kkk <a5p1k@yandex.ru>
2024-03-19 19:11:59 +0300

    queries path created and 2 queries added

---
+++ database/queries/queries.py
@@ -0,0 +1,14 @@
+createQuery = """
+	CREATE TABLE "restaurants" (
+		"id"	INTEGER NOT NULL UNIQUE,
+		"name"	INTEGER,
+		"rating" NUMERIC(2,1),
+		"bill"	TEXT,
+		"tags" TEXT,
+		PRIMARY KEY("id" AUTOINCREMENT)
+	);
+	"""
+
+insertQuery = """
+INSERT INTO restaurants VALUES (?, ?, ?, ?)
+"""
