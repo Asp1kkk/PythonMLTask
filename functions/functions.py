7895a5d0618d978695980079ca7efeb9cc1a9a59
Asp1kkk <a5p1k@yandex.ru>
2024-03-19 17:52:36 +0300

    getName func created

--- functions/functions.py
+++ functions/functions.py
@@ -4,3 +4,6 @@ def getAverageBill(item: bs4.element.Tag) -> str:
 	bill_span = item.find("span", {"class": "average-bill"})
 	average_bill = bill_span.get_text().replace(".", "") if bill_span != None else None
 	return average_bill
+
+def getName(item: bs4.element.Tag) -> str:
+	return item.find("a", {"class": "name"}).get_text()
