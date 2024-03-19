86bbf5ed2ac8861a36a6c2811302fcedc8cf1872
Asp1kkk <a5p1k@yandex.ru>
2024-03-19 18:06:27 +0300

    getTags func created

--- functions/functions.py
+++ functions/functions.py
@@ -7,3 +7,12 @@ def getAverageBill(item: bs4.element.Tag) -> str:
 
 def getName(item: bs4.element.Tag) -> str:
 	return item.find("a", {"class": "name"}).get_text()
+
+def getTags(item: bs4.element.Tag) -> list:
+	props_wrap_div = item.find("div", {"class": "props-wrap"})
+	cuisine_span = props_wrap_div.find("span", string="Кухня: ")
+	cuisine_text = cuisine_span.find_parent().get_text()
+	cuisine_text = cuisine_text.replace("Кухня:  ", "").replace('\xa0', ' ')
+	cuisine_list = cuisine_text.split(', ')
+	
+	return cuisine_list
