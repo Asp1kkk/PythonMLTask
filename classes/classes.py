2a75eac882493e5a3f3db772cf15faf72d6ef16e
Asp1kkk <a5p1k@yandex.ru>
2024-03-19 16:46:42 +0300

    Class resaturant

---
+++ classes/classes.py
@@ -0,0 +1,6 @@
+class Restaurant:
+	def __init__(self, name: str, tags: list, average: str, rate: float) -> None:
+		self.name = name
+		self.tags = tags
+		self.average = average
+		self.rate = rate
